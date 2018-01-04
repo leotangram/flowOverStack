081734e51635c3b84c9011811851c2f3a8e98512
leotangram <leotangram@gmail.com>
2018-01-04 18:22:54 -0500

    Agregar alerta para notice

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -6,4 +6,18 @@ class QuestionsController < ApplicationController
 	def new
 		@question = Question.new
 	end
+
+	def create
+	  @question = Question.new(question_params)
+	  if @question.save
+	    redirect_to questions_path, notice: "La pregunta fue publicada con éxito"
+	  else
+	    render :new
+	  end
+	end
+
+	private
+	  def question_params
+	    params.require(:question).permit(:title, :description)
+	  end
 end
