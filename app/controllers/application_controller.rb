60496e13d5f438c05b150baf9642e2f734b198b0
leotangram <leotangram@gmail.com>
2018-01-04 21:39:11 -0500

    Restringe las páginas privadas

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  private
+	  def private_access
+	  	redirect_to new_user_session_path, notice: "Nos hemos dado cuenta que estás tratando de hacer algo que no debes hacer, ¡ten cuidado! Te estamos vigilando :O" unless signed_in?
+	  end
 end
