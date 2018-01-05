b94ed19674a7b2cfb51515032ac206cefa53f8e9
leotangram <leotangram@gmail.com>
2018-01-05 17:55:37 -0500

    Anota los modelos con los campos de la base de datos

--- test/models/question_test.rb
+++ test/models/question_test.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: questions
+#
+#  id          :integer          not null, primary key
+#  title       :string
+#  description :text
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 require 'test_helper'
 
 class QuestionTest < ActiveSupport::TestCase
